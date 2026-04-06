be32b2bc8cc611f5072b0e68a22fd67e26fb0468
“Tengereeb8” <tengererdenebattulga8@gmail.com>
2026-04-06 12:34:38 +0800

    c

--- admin/app/server/auth/getUser.ts
+++ admin/app/server/auth/getUser.ts
@@ -1,20 +1,20 @@
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 
-// export const getUser = async () => {
-//   const cookieStore = await cookies();
+export const getUser = async () => {
+  const cookieStore = await cookies();
 
-//   const token = cookieStore.get("token")?.value;
+  const token = cookieStore.get("token")?.value;
 
-//   const response = await fetch("http://localhost:3001/users/profile", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
+  const response = await fetch("http://localhost:3001/users/profile", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-//   const data = await response.json();
-//   console.log(data);
+  const data = await response.json();
+  console.log(data);
 
-//   return data;
-// };
+  return data;
+};
